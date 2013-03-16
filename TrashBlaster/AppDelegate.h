fd1f086cd292b23ecf79ff572aa6473c23dbe4aa
Weston Cannon Beecroft <weston@Westons-MacBook-Pro.local>
2013-03-16 04:41:59 -0400

    Initial Commit

---
+++ TrashBlaster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrashBlaster
+//
+//  Created by Weston Cannon Beecroft on 3/16/13.
+//  Copyright (c) 2013 Weston Cannon Beecroft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrashBlaster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TrashBlaster
+//
+//  Created by Weston Cannon Beecroft on 3/16/13.
+//  Copyright (c) 2013 Weston Cannon Beecroft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
